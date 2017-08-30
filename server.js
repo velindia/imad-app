a825dd050033b053ba3484c8875000669d22345f
Thangavelu <velindia502@gmail.com>
2017-08-30 12:51:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
-});
 
 app.get('/contact.html',function (req,res){
-    res.send('i am here');
+    res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
 
 });
 app.get('/ui/style.css', function (req, res) {
