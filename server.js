02dd7127fa5ca6245a0c03406bb6786926951279
Thangavelu <velindia502@gmail.com>
2017-08-30 12:20:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/contact',function(req,res){
+    res.send('contact me on 8015687842');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
